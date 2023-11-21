da20f8b2510c446014dfae86d83a79c0c316c0ec
Dylan Depass <dylandepass@gmail.com>
2023-11-21 13:32:11 -0500

    fix: missing process.env when running tests

--- web-test-runner.config.cjs
+++ web-test-runner.config.cjs
@@ -18,4 +18,11 @@ module.exports = {
     report: true,
     reportDir: 'coverage-wtr',
   },
+  testRunnerHtml: testFramework => `
+  <html>
+    <body>
+      <script>window.process = { env: { NODE_ENV: "development" } }</script>
+      <script type="module" src="${testFramework}"></script>
+    </body>
+  </html>`,
 };
