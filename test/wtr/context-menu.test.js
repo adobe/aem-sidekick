d39cd9d95702dbfb635c1f75579058a56ea68551
rofe <github@rofe.com>
2023-12-27 15:57:16 +0100

    chore(context-menu): 100% coverage

--- test/wtr/context-menu.test.js
+++ test/wtr/context-menu.test.js
@@ -17,9 +17,22 @@ import sinon from 'sinon';
 
 import chromeMock from './mocks/chrome.js';
 import { addProject, getProject, updateProject } from '../../src/extension/project.js';
+import { internalActions } from '../../src/extension/actions.js';
 
 // @ts-ignore
 window.chrome = chromeMock;
+
+// prep listener test before importing context-menu.js
+const openPreview = sinon.spy(internalActions, 'openPreview');
+sinon.replace(chrome.contextMenus.onClicked, 'addListener', async (func) => {
+  // @ts-ignore
+  func({
+    menuItemId: 'openPreview',
+  }, {
+    id: 1,
+    url: 'https://main--blog--adobe.hlx.page/',
+  });
+});
 
 const { updateContextMenu } = await import('../../src/extension/context-menu.js');
 
@@ -34,6 +47,10 @@ describe('Test context-menu', () => {
     sandbox.restore();
   });
 
+  it('click listener invokes action', async () => {
+    expect(openPreview.called).to.be.true;
+  });
+
   it('updateContextMenu', async () => {
     const removeAll = sandbox.spy(chrome.contextMenus, 'removeAll');
     const create = sandbox.spy(chrome.contextMenus, 'create');
@@ -41,29 +58,52 @@ describe('Test context-menu', () => {
       owner: 'foo',
       repo: 'bar',
     };
-    // unknown project
+
+    // project not added yet (remove all and add 1)
     await updateContextMenu({
       id: 0,
       url: 'https://main--bar--foo.hlx.page/',
       config,
     });
-    expect(removeAll.calledOnce).to.be.true;
-    expect(create.calledOnce).to.be.true;
-    // project disabled
+    expect(removeAll.callCount).to.equal(1);
+    expect(create.callCount).to.equal(1);
+
+    // project added (remove all and add 3)
     await addProject(config);
     const project = await getProject(config);
+    await updateContextMenu({
+      id: 0,
+      url: 'https://main--bar--foo.hlx.page/',
+      config,
+    });
+    expect(removeAll.callCount).to.equal(2);
+    expect(create.callCount).to.equal(4);
+
+    // project disabled (remove all and add 3)
     await updateProject({ ...project, disabled: true });
-    // no config
+    await updateContextMenu({
+      id: 0,
+      url: 'https://main--bar--foo.hlx.page/',
+      config,
+    });
+    expect(removeAll.callCount).to.equal(3);
+    expect(create.callCount).to.equal(7);
+
+    // no config (remove all and add 0)
     await updateContextMenu({
       id: 0,
       url: 'https://main--bar--foo.hlx.page/',
     });
-    expect(removeAll.calledTwice).to.be.true;
-    // not implemented
+    expect(removeAll.callCount).to.equal(4);
+    expect(create.callCount).to.equal(7);
+
+    // chrome.contextMenus not implemented
     removeAll.resetHistory();
+    const originalContextMenus = chrome.contextMenus;
     delete chrome.contextMenus;
     // @ts-ignore
     await updateContextMenu({});
     expect(removeAll.called).to.be.false;
+    chrome.contextMenus = originalContextMenus;
   });
 });
