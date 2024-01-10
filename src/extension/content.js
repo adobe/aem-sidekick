8e2cf7699fdd11e1ad8e8688d076a5b3c16f2ef4
rofe <github@rofe.com>
2024-01-09 16:24:19 -0800

    fix: re-entry check

--- src/extension/content.js
+++ src/extension/content.js
@@ -16,7 +16,7 @@
 
   const { getDisplay } = await import('./display.js');
   const display = await getDisplay();
-  let { sidekick } = window.hlx;
+  let sidekick = document.querySelector('aem-sidekick');
   if (!sidekick) {
     // wait for config matches
     chrome.runtime.onMessage.addListener(async ({ configMatches = [] }, { tab }) => {
@@ -30,37 +30,35 @@
         // console.log('single match', config);
         await import('./lib/polyfills.min.js');
 
-        if (!sidekick) {
-          const { AEMSidekick } = await import('./index.js');
+        const { AEMSidekick } = await import('./index.js');
 
-          // reduce config to only include properties relevant for sidekick
-          const curatedConfig = Object.fromEntries(Object.entries(config)
-            .filter(([k]) => [
-              'owner',
-              'repo',
-              'ref',
-              'previewHost',
-              'liveHost',
-              'host',
-              'devMode',
-              'devOrigin',
-              'adminVersion',
-              'authTokenExpiry',
-              'hlx5',
-            ].includes(k)));
-          curatedConfig.scriptUrl = chrome.runtime.getURL('index.js');
+        // reduce config to only include properties relevant for sidekick
+        const curatedConfig = Object.fromEntries(Object.entries(config)
+          .filter(([k]) => [
+            'owner',
+            'repo',
+            'ref',
+            'previewHost',
+            'liveHost',
+            'host',
+            'devMode',
+            'devOrigin',
+            'adminVersion',
+            'authTokenExpiry',
+            'hlx5',
+          ].includes(k)));
+        curatedConfig.scriptUrl = chrome.runtime.getURL('index.js');
 
-          sidekick = new AEMSidekick(curatedConfig);
-          sidekick.setAttribute('open', display);
-          document.body.prepend(sidekick);
-          window.hlx.sidekick = sidekick;
-        }
+        sidekick = new AEMSidekick(curatedConfig);
+        sidekick.setAttribute('open', `${display}`);
+        document.body.prepend(sidekick);
+        window.hlx.sidekick = sidekick;
       } else {
         // todo: multiple matches, project picker?
         // console.log('multiple matches', configMatches);
       }
     });
   } else {
-    sidekick.setAttribute('open', display);
+    sidekick.setAttribute('open', `${display}`);
   }
 })();
