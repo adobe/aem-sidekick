558ef03eea048ac5c1408154cc1416de614ae185
rofe <github@rofe.com>
2024-01-12 17:40:55 -0800

    chore: no safari release

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -2,13 +2,14 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ['semantic-release-react-native', {
-      'versionStrategy': {
-        'ios': { 'buildNumber': 'semantic' },
-      },
-      'iosPath': 'src/safari',
-      'skipAndroid': true,
-    }],
+    // todo: add back when we have a safari project
+    // ['semantic-release-react-native', {
+    //   'versionStrategy': {
+    //     'ios': { 'buildNumber': 'semantic' },
+    //   },
+    //   'iosPath': 'src/safari',
+    //   'skipAndroid': true,
+    // }],
     ['@semantic-release/changelog', {
       'changelogFile': 'CHANGELOG.md',
     }],
