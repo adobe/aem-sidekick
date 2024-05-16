ea961fd029358eef24308558b40ec7287c4b64dd
rofe <github@rofe.com>
2024-05-16 14:07:03 +0200

    refactor: implement feedback

--- src/extension/app/utils/helix-admin.js
+++ src/extension/app/utils/helix-admin.js
@@ -19,124 +19,23 @@
  */
 
 /**
- * Enumeration of rate limiting sources.
- * @private
- * @type {Object<string, string>}}
+ * The origin of the Admin API.
  */
-const RATE_LIMITER = {
-  NONE: '',
-  ADMIN: 'AEM',
-  ONEDRIVE: 'Microsoft SharePoint',
-};
-
-/**
- * Returns an error message from the server response.
- * @param {Response} resp The response
- * @returns {string} The error message or an empty string
- */
-export function getServerError(resp) {
-  return resp?.headers?.get('x-error') || '';
-}
-
-/**
- * Checks if the request has been rate-limited and returns the source.
- * @param {Response} resp The response
- * @returns {string} The rate limiter (see {@link RATE_LIMITER})
- */
-function getRateLimiter(resp) {
-  if (resp.status === 429) {
-    return RATE_LIMITER.ADMIN;
-  }
-  const error = getServerError(resp);
-  if (resp.status === 503 && error.includes('(429)')) {
-    if (error.includes('onedrive')) {
-      return RATE_LIMITER.ONEDRIVE;
-    }
-  }
-  return RATE_LIMITER.NONE;
-}
-
-/**
- * Returns the action name based on API and nature of request.
- * @param {string} api The API endpoint called
- * @param {boolean} [del] True if the request was destructive
- */
-function getAction(api, del) {
-  let action = api;
-  if (api === 'preview' && del) {
-    action = 'delete';
-  }
-  if (api === 'live') {
-    action = del ? 'unpublish' : 'publish';
-  }
-  return action;
-}
-
-/**
- * Shows a toast if the server returned an error.
- * @param {AppStore} appStore The app store
- * @param {string} action The action
- * @param {Response} resp The response object
- */
-function handleServerError(appStore, action, resp) {
-  if (resp.ok) {
-    return;
-  }
-
-  const limiter = getRateLimiter(resp);
-  if (limiter) {
-    appStore.showToast(appStore.i18n('error_429').replace('$1', limiter), 'warning');
-  }
-
-  let message = '';
-  if (action === 'status' && resp.status === 404) {
-    // special handling for status
-    message = appStore.i18n(appStore.isEditor()
-      ? 'error_status_404_document'
-      : 'error_status_404_content');
-  } else {
-    // error key fallbacks
-    message = appStore.i18n(`error_${action}_${resp.status}`)
-      || appStore.i18n(`error_${action}`)
-      || appStore.i18n('error_fatal');
-  }
-
-  appStore.showToast(
-    message.replace('$1', getServerError(resp)),
-    resp.status < 500 ? 'warning' : 'negative',
-    () => appStore.closeToast(),
-  );
-}
-
-/**
- * Shows a toast if the request failed or did not contain valid JSON.
- * @param {AppStore} appStore The app store
- * @param {string} action The action
- */
-function handleFatalError(appStore, action) {
-  // use standard error key fallbacks
-  const msg = appStore.i18n(`error_${action}_fatal`)
-      || appStore.i18n('error_fatal');
-  appStore.showToast(
-    msg.replace('$1', 'https://aemstatus.net/'),
-    'negative',
-    () => appStore.closeToast(),
-  );
-}
+export const ADMIN_ORIGIN = 'https://admin.hlx.page';
 
 /**
  * Creates an Admin URL for an API and path.
- * @param {SiteStore} siteStore The sidekick config object
+ * @param {Object} config The sidekick config object
  * @param {string} api The API endpoint to call
  * @param {string} path The current path
  * @returns {URL} The admin URL
  */
 export function getAdminUrl({
-  owner, repo, ref, adminVersion,
+  owner, repo, ref = 'main', adminVersion,
 }, api, path = '', searchParams = new URLSearchParams()) {
   const adminUrl = new URL([
-    'https://admin.hlx.page/',
-    api,
+    ADMIN_ORIGIN,
+    `/${api}`,
     `/${owner}`,
     `/${repo}`,
     `/${ref}`,
@@ -152,44 +51,172 @@ export function getAdminUrl({
 }
 
 /**
- * Calls an Admin URL for an API and path and returns the response.
- * @param {AppStore} appStore The app store
- * @param {Object} opts The options
- * @param {string} [opts.api] The API endpoint to call
- * @param {string} [opts.path] The resource path
- * @param {URLSearchParams} [opts.searchParams] The search parameters
- * @param {string} [opts.method] The method to use
- * @param {Object} [opts.body] The body to send
- * @param {boolean} [opts.omitCredentials] Should we omit the credentials
- * @returns {Promise<Response>} The admin response
- */
-async function callAdmin(appStore, {
-  api = 'status',
-  path = '',
-  searchParams = new URLSearchParams(),
-  method = 'get',
-  body,
-  omitCredentials = false,
-}) {
-  const url = getAdminUrl(appStore.siteStore, api, path, searchParams);
-  return fetch(url, {
-    cache: 'no-store',
-    credentials: omitCredentials ? 'omit' : 'include',
-    headers: {},
-    method,
-    body,
-  });
-}
-
-/**
- * Admin API allows interaction with the [AEM Admin API]{@link https://www.aem.live/docs/admin.html}.
- * @param {AppStore} appStore The app store
- * @returns {AdminAPI} The Admin API
+ * Admin API interacts with the [AEM Admin API]{@link https://www.aem.live/docs/admin.html}.
  */
 export class AdminAPI {
+  /**
+   * Enumeration of rate limiting sources.
+   * @private
+   * @type {Object<string, string>}}
+   */
+  RATE_LIMITER = {
+    NONE: '',
+    ADMIN: 'AEM',
+    ONEDRIVE: 'Microsoft SharePoint',
+  };
+
+  /**
+   * The app store.
+   * @private
+   * @type {AppStore}
+   */
+  appStore;
+
+  /**
+   * The site store
+   * @private
+   * @type {SiteStore}
+   */
+  siteStore;
+
+  /**
+   * Returns an Admin API.
+   * @param {AppStore} appStore The app store
+   */
   constructor(appStore) {
     this.appStore = appStore;
     this.siteStore = appStore.siteStore;
+  }
+
+  /**
+   * Returns an error message from the server response.
+   * @private
+   * @param {Response} resp The response
+   * @returns {string} The error message or an empty string
+   */
+  getServerError(resp) {
+    return resp?.headers?.get('x-error') || '';
+  }
+
+  /**
+   * Checks if the request has been rate-limited and returns the source.
+   * @param {Response} resp The response
+   * @returns {string} The rate limiter (see {@link RATE_LIMITER})
+   */
+  getRateLimiter(resp) {
+    if (resp.status === 429) {
+      return this.RATE_LIMITER.ADMIN;
+    }
+    const error = this.getServerError(resp);
+    if (resp.status === 503 && error.includes('(429)')) {
+      if (error.includes('onedrive')) {
+        return this.RATE_LIMITER.ONEDRIVE;
+      }
+    }
+    return this.RATE_LIMITER.NONE;
+  }
+
+  /**
+   * Returns the action name based on API and nature of request.
+   * @private
+   * @param {string} api The API endpoint called
+   * @param {boolean} [del] True if the request was destructive
+   */
+  getAction(api, del) {
+    let action = api;
+    if (api === 'preview' && del) {
+      action = 'delete';
+    }
+    if (api === 'live') {
+      action = del ? 'unpublish' : 'publish';
+    }
+    return action;
+  }
+
+  /**
+   * Shows a toast if the server returned an error.
+   * @private
+   * @param {string} action The action
+   * @param {Response} resp The response object
+   */
+  handleServerError(action, resp) {
+    if (resp.ok) {
+      return;
+    }
+
+    // handle rate limiting
+    const limiter = this.getRateLimiter(resp);
+    if (limiter) {
+      this.appStore.showToast(
+        this.appStore.i18n('error_429').replace('$1', limiter),
+        'warning',
+      );
+    }
+
+    let message = '';
+    if (action === 'status' && resp.status === 404) {
+      // special handling for status
+      message = this.appStore.i18n(this.appStore.isEditor()
+        ? 'error_status_404_document'
+        : 'error_status_404_content');
+    } else {
+      // error key fallbacks
+      message = this.appStore.i18n(`error_${action}_${resp.status}`)
+        || this.appStore.i18n(`error_${action}`)
+        || this.appStore.i18n('error_fatal');
+    }
+
+    this.appStore.showToast(
+      message.replace('$1', this.getServerError(resp)),
+      resp.status < 500 ? 'warning' : 'negative',
+      () => this.appStore.closeToast(),
+    );
+  }
+
+  /**
+   * Shows a toast if the request failed or did not contain valid JSON.
+   * @private
+   * @param {string} action The action
+   */
+  handleFatalError(action) {
+    // use standard error key fallbacks
+    const msg = this.appStore.i18n(`error_${action}_fatal`)
+        || this.appStore.i18n('error_fatal');
+    this.appStore.showToast(
+      msg.replace('$1', 'https://aemstatus.net/'),
+      'negative',
+      () => this.appStore.closeToast(),
+    );
+  }
+
+  /**
+   * Calls an Admin URL for an API and path and returns the response.
+   * @private
+   * @param {Object} opts The options
+   * @param {string} [opts.api] The API endpoint to call
+   * @param {string} [opts.path] The resource path
+   * @param {URLSearchParams} [opts.searchParams] The search parameters
+   * @param {string} [opts.method] The method to use
+   * @param {Object} [opts.body] The body to send
+   * @param {boolean} [opts.omitCredentials] Should we omit the credentials
+   * @returns {Promise<Response>} The admin response
+   */
+  async callAdmin({
+    api = 'status',
+    path = '',
+    searchParams = new URLSearchParams(),
+    method = 'get',
+    body,
+    omitCredentials = false,
+  }) {
+    const url = getAdminUrl(this.siteStore, api, path, searchParams);
+    return fetch(url, {
+      cache: 'no-store',
+      credentials: omitCredentials ? 'omit' : 'include',
+      headers: {},
+      method,
+      body,
+    });
   }
 
   /**
@@ -202,7 +229,7 @@ export class AdminAPI {
   async getStatus(path, includeEditUrl = false) {
     let resp;
     try {
-      resp = await callAdmin(this.appStore, {
+      resp = await this.callAdmin({
         path,
         searchParams: includeEditUrl ? new URLSearchParams(
           [['editUrl', this.appStore.isEditor() ? this.appStore.location.href : 'auto']],
@@ -213,10 +240,35 @@ export class AdminAPI {
       } else if (resp.status === 401) {
         return { status: 401 };
       } else {
-        handleServerError(this.appStore, getAction('status'), resp);
+        this.handleServerError(this.getAction('status'), resp);
       }
     } catch (e) {
-      handleFatalError(this.appStore, getAction('status'));
+      this.handleFatalError(this.getAction('status'));
+    }
+    return null;
+  }
+
+  /**
+   * Returns the sidekick config.
+   * @see https://www.aem.live/docs/admin.html#tag/sidekick
+   * @returns {Promise<Object>} The JSON response
+   */
+  async getSidekickConfig() {
+    let resp;
+    try {
+      resp = await this.callAdmin({
+        api: 'sidekick',
+        path: '/config.json',
+      });
+      if (resp.ok) {
+        return resp.json();
+      } else if (resp.status === 401) {
+        return { status: 401 };
+      } else {
+        this.handleServerError(this.getAction('status'), resp);
+      }
+    } catch (e) {
+      this.handleFatalError(this.getAction('status'));
     }
     return null;
   }
@@ -228,7 +280,7 @@ export class AdminAPI {
    */
   async getProfile() {
     try {
-      const resp = await callAdmin(this.appStore, {
+      const resp = await this.callAdmin({
         api: 'profile',
       });
       if (resp.ok) {
@@ -251,7 +303,7 @@ export class AdminAPI {
     const api = 'preview';
     const method = del ? 'delete' : 'post';
     try {
-      const resp = await callAdmin(this.appStore, {
+      const resp = await this.callAdmin({
         path,
         api,
         method,
@@ -259,10 +311,10 @@ export class AdminAPI {
       if (resp.ok) {
         return resp.json();
       } else {
-        handleServerError(this.appStore, getAction(api, del), resp);
+        this.handleServerError(this.getAction(api, del), resp);
       }
     } catch (e) {
-      handleFatalError(this.appStore, getAction(api, del));
+      this.handleFatalError(this.getAction(api, del));
     }
     return null;
   }
@@ -278,7 +330,7 @@ export class AdminAPI {
     const api = 'live';
     const method = del ? 'delete' : 'post';
     try {
-      const resp = await callAdmin(this.appStore, {
+      const resp = await this.callAdmin({
         path,
         api,
         method,
@@ -286,10 +338,10 @@ export class AdminAPI {
       if (resp.ok) {
         return resp.json();
       } else {
-        handleServerError(this.appStore, getAction(api, del), resp);
+        this.handleServerError(this.getAction(api, del), resp);
       }
     } catch (e) {
-      handleFatalError(this.appStore, getAction(api, del));
+      this.handleFatalError(this.getAction(api, del));
     }
     return null;
   }
