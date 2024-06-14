ee245a97fc737a50bf04944a399a9c461a538162
Dylan Depass <dylandepass@gmail.com>
2024-06-14 10:37:16 -0400

    fix: update clickToastClose to be asynchronous
    Changed the clickToastClose function to be awaited for proper execution.

--- test/app/plugins/delete/delete.test.js
+++ test/app/plugins/delete/delete.test.js
@@ -185,7 +185,7 @@ describe('Delete plugin', () => {
 
         expect(showToastSpy.calledOnce).to.be.true;
 
-        sidekickTest.clickToastClose();
+        await sidekickTest.clickToastClose();
         expect(reloadPageStub.calledOnce).to.be.false;
       });
 
