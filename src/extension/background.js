d6df90cf9ec7c1bc405134c753848ea5b5adc10a
rofe <github@rofe.com>
2024-08-09 17:42:37 +0200

    chore: send message to old sidekick

--- src/extension/background.js
+++ src/extension/background.js
@@ -80,3 +80,12 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
 addAuthTokenHeaders();
 
 log.info('sidekick initialized');
+console.log('new sidekick');
+
+chrome.runtime.sendMessage(
+  'olciodlegapfmemadcjicljalmfmlehb',
+  { action: 'getProjects' },
+  (response) => {
+    console.log('response from old sidekick', response);
+  },
+);
