7e8e1fc7896f44b98cab0bebd6c3551edc9b1d92
Raphael Wegmueller <github@rofe.com>
2024-09-24 10:59:25 +0200

    feat(publish): add confirm option (#262)

--- src/extension/types/typedefs.js
+++ src/extension/types/typedefs.js
@@ -120,10 +120,10 @@
  * @prop {string} id The plugin ID (mandatory)
  * @prop {PluginButton} [button] A button configuration object (optional)
  * @prop {string} [container] The ID of a dropdown to add this plugin to (optional)
- * @prop {boolean} [override] Replace an existing plugin (optional)
  * @prop {ElemConfig[]} [elements]  An array of elements to add (optional)
  * @prop {Function} [condition] Show this plugin (optional).
  * @prop {boolean} [pinned=true] Pin this plugin to the action bar (optional).
+ * @prop {boolean} [confirm=false] Requires confirmation before action is executed (optional).
  * This function is expected to return a boolean when called with the sidekick as argument.
  * @prop {Function} [callback] A function called after adding the plugin (optional).
  * This function is called with the sidekick and the newly added plugin as arguments.
