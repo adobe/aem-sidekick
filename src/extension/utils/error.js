bf011d7fefcfd343ac4e448e1735496cea3e8909
Ben Peter <benp@adobe.com>
2026-09-07 10:15:50 +0200

    fix: defer mixer site authentication

--- src/extension/utils/error.js
+++ src/extension/utils/error.js
@@ -20,7 +20,6 @@ export function isErrorPage(location, document) {
   return !!((location.host.endsWith('.aem.page')
     || location.host.endsWith('.aem.live')
     || location.host.endsWith('.aem.reviews')
-    || location.host.endsWith('.aem.network')
     || location.hostname === 'localhost')
     && !document.querySelector('body > main > div')
     && document.querySelector('body > pre'));
